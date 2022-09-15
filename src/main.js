7673687b7ff66b883c4eac52a7232ab64e713ff4
leap <leap@localhost.localdomain>
2022-09-16 01:53:53 +0500

    rm debug

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import { Bitrix24 } from './install';
 
-Bitrix24.init(['https://code.jquery.com/jquery-3.6.1.min.js', 'https://code.jquery.com/jquery-3.6.0.min.js']).then((BX24) => {
+Bitrix24.init().then((BX24) => {
   createApp(App).provide('$BX24', BX24).mount('#app');
 });
