fb930d75d55f5bf2f1cae50eac54523602228136
Валентин <astrotrain55@gmail.com>
2022-10-25 02:05:44 +0500

    fix load default styles

--- src/lib/plugin.js
+++ src/lib/plugin.js
@@ -2,10 +2,10 @@ import { loadScripts } from '../utils';
 import { Bitrix24 } from './Bitrix24';
 
 export default {
-  init(...scripts) {
-    return loadScripts('//api.bitrix24.com/api/v1/', ...scripts).then(() => {
+  init(scripts, css = true) {
+    return loadScripts('//api.bitrix24.com/api/v1/', scripts).then(() => {
       if (window.BX24) {
-        this.initAssets(window.BX24.getDomain());
+        if (css) this.initAssets(window.BX24.getDomain());
         const BX24 = new Bitrix24(window.BX24);
         return BX24.init().then(() => BX24);
       }
