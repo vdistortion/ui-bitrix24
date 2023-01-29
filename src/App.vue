ba4e9b45b76e346a02c81ab410ba9e8572588445
Валентин <astrotrain55@gmail.com>
2023-01-29 18:43:58 +0500

    update

--- src/App.vue
+++ src/App.vue
@@ -61,7 +61,7 @@ export default {
   },
   data() {
     return {
-      componentVisible: 'bx-dialog',
+      componentVisible: 'bx-button',
       components: [
         'bx-alert',
         'bx-button',
