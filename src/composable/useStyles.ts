556089ecb9c01bb905ff32a18c7d88cdf5f3c639
Валентин <astrotrain55@gmail.com>
2024-06-03 14:05:17 +0500

    fix param useStyles

--- src/composable/useStyles.ts
+++ src/composable/useStyles.ts
@@ -2,7 +2,7 @@ import { inject } from 'vue';
 import type { IBitrix24Library } from 'bitrix24-library';
 import { loadStyles } from '../utils/loadStyles';
 
-export function useStyles(key: '$BX24') {
+export function useStyles(key = '$BX24') {
   const BX24: IBitrix24Library | undefined = inject(key);
   loadStyles(BX24);
 }
