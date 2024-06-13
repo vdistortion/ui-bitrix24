f954c6b42834e072840317ad1ad0ef82638c198e
Валентин <astrotrain55@gmail.com>
2024-06-13 17:50:13 +0500

    refactor types bx-progressbar

--- src/components/BxProgressbar.vue
+++ src/components/BxProgressbar.vue
@@ -8,73 +8,62 @@
   </div>
 </template>
 
-<script lang="ts">
-export type PropSizes = 'md' | 'lg';
-export type PropColors = 'primary' | 'success' | 'warning' | 'danger';
-
-export type TypesProps = {
-  sizes: PropSizes[];
-  colors: PropColors[];
-};
-
-export const propsValues: TypesProps = {
-  sizes: ['md', 'lg'],
-  colors: ['primary', 'success', 'warning', 'danger'],
-};
-</script>
-
 <script setup lang="ts">
 import { computed, type PropType } from 'vue';
+import { defaultProps, type PropColor, type PropSize, propsValues } from './BxProgressbar.props';
 import { useStyles } from '../composable/useStyles';
+import { getClassName } from '../utils/getClassName';
 
 useStyles();
 
 const props = defineProps({
   progress: {
     type: Number,
-    default: 0,
+    default: defaultProps.progress,
     validator(value) {
       return Number.isFinite(value);
     },
   },
   size: {
-    type: String as PropType<PropSizes>,
-    default: 'md',
-    validator(value: PropSizes) {
+    type: String as PropType<PropSize>,
+    default: defaultProps.size,
+    validator(value: PropSize) {
       return propsValues.sizes.includes(value);
     },
   },
   color: {
-    type: String as PropType<PropColors>,
-    default: 'primary',
-    validator(value: PropColors) {
+    type: String as PropType<PropColor>,
+    default: defaultProps.color,
+    validator(value: PropColor) {
       return propsValues.colors.includes(value);
     },
   },
   textBefore: {
     type: String,
-    default: '',
+    default: defaultProps.textBefore,
   },
   textAfter: {
     type: String,
-    default: '',
+    default: defaultProps.textAfter,
   },
   bg: {
     type: Boolean,
-    default: false,
+    default: defaultProps.bg,
   },
   column: {
     type: Boolean,
-    default: false,
+    default: defaultProps.column,
   },
 });
 
 const width = computed(() => `${props.progress}%`);
 
-const classList = computed(() => ({
-  [`ui-progressbar-${props.size}`]: propsValues.sizes.includes(props.size),
-  [`ui-progressbar-${props.color}`]: propsValues.colors.includes(props.color),
-  'ui-progressbar-bg': props.bg,
-  'ui-progressbar-column': props.column,
-}));
+const classList = computed(() =>
+  getClassName({
+    [`ui-progressbar-${props.size}`]: propsValues.sizes.includes(props.size),
+    [`ui-progressbar-${props.color}`]: propsValues.colors.includes(props.color),
+    'ui-progressbar-bg': props.bg,
+    'ui-progressbar-column': props.column,
+  }),
+);
 </script>
--- src/stories/BxProgressbar.stories.ts
+++ src/stories/BxProgressbar.stories.ts
@@ -1,31 +1,9 @@
 import type { Meta, StoryObj } from '@storybook/vue3';
-import BxProgressbar, {
-  propsValues,
-  type PropSizes,
-  type PropColors,
-} from '../components/BxProgressbar.vue';
+import { Story } from './Story';
+import BxProgressbar from '../components/BxProgressbar.vue';
+import { defaultProps, propsValues, type TypesPropsList } from '../components/BxProgressbar.props';
 
-type DefaultProps = {
-  progress: number;
-  size: PropSizes;
-  color: PropColors;
-  textBefore: string;
-  textAfter: string;
-  bg: boolean;
-  column: boolean;
-};
-
-const defaultProps: DefaultProps = {
-  progress: 0,
-  size: 'md',
-  color: 'primary',
-  textBefore: '',
-  textAfter: '',
-  bg: false,
-  column: false,
-};
-
-const meta = {
+const meta: Meta<typeof BxProgressbar> = {
   title: 'bx-progressbar',
   component: BxProgressbar,
   args: {
@@ -39,74 +17,34 @@ const meta = {
   },
   argTypes: {
     progress: {
-      defaultValue: defaultProps.progress,
       control: { type: 'number', min: 0, max: 100 },
     },
     size: {
       options: propsValues.sizes,
-      defaultValue: defaultProps.size,
       control: { type: 'inline-radio' },
     },
     color: {
       options: propsValues.colors,
-      defaultValue: defaultProps.color,
       control: { type: 'inline-radio' },
     },
     textBefore: {
-      defaultValue: defaultProps.textBefore,
       control: { type: 'text' },
     },
     textAfter: {
-      defaultValue: defaultProps.textAfter,
       control: { type: 'text' },
     },
     bg: {
-      defaultValue: defaultProps.bg,
       control: { type: 'boolean' },
     },
     column: {
-      defaultValue: defaultProps.column,
       control: { type: 'boolean' },
     },
   },
-} satisfies Meta<typeof BxProgressbar>;
-
-export default meta;
+};
 
 type StoryType = StoryObj<typeof meta>;
 
-export const Default: StoryType = {
-  render: (args) => ({
-    template: '<bx-progressbar v-bind="args" v-on="args"></bx-progressbar>',
-    data: () => ({ args }),
-    provide: {
-      $BX24: null,
-    },
-    components: { BxProgressbar },
-  }),
-};
-
-const Story = (propName: string, propList: any[]) => ({
-  render: (args: any) => ({
-    template: `
-      <div v-for="item in propList" :key="item" class="component">
-        <bx-progressbar v-bind="args" v-on="args" :[propName]="item"></bx-progressbar>
-      </div>
-    `,
-    data: () => ({ args, propName, propList }),
-    provide: {
-      $BX24: null,
-    },
-    components: { BxProgressbar },
-  }),
-  argTypes: {
-    [propName]: {
-      table: {
-        disable: true,
-      },
-    },
-  },
-});
-
-export const Colors: StoryType = Story('color', propsValues.colors);
-export const Sizes: StoryType = Story('size', propsValues.sizes);
+export default meta;
+export const Default: StoryType = Story(BxProgressbar);
+export const Colors: StoryType = Story<TypesPropsList>(BxProgressbar, 'color', propsValues.colors);
+export const Sizes: StoryType = Story<TypesPropsList>(BxProgressbar, 'size', propsValues.sizes);
