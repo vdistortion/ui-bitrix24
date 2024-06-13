4795b37b445e7ebcd5e923e98a7cf61c586d3b8f
Валентин <astrotrain55@gmail.com>
2024-06-13 16:45:31 +0500

    refactor types bx-icon

--- src/components/BxIcon.vue
+++ src/components/BxIcon.vue
@@ -1,403 +1,50 @@
 <template>
-  <div class="ui-icon" :class="classList">
+  <button v-if="button" class="ui-icon" :class="classList">
+    <i></i>
+  </button>
+  <div v-else class="ui-icon" :class="classList">
     <i></i>
   </div>
 </template>
 
-<script lang="ts">
-export type PropIcons =
-  | 'file-empty'
-  | 'file-txt'
-  | 'file-doc'
-  | 'file-xls'
-  | 'file-php'
-  | 'file-pdf'
-  | 'file-ppt'
-  | 'file-rar'
-  | 'file-zip'
-  | 'file-set'
-  | 'file-img'
-  | 'file-mov'
-  | 'file-folder'
-  | 'file-folder-shared'
-  | 'file-folder-shared-2'
-  | 'common-user'
-  | 'common-user-group'
-  | 'common-company'
-  | 'common-phone'
-  | 'common-notification'
-  | 'common-bitrix24'
-  | 'common-cloud'
-  | 'common-folder'
-  | 'common-info'
-  | 'service-alice'
-  | 'service-light-alice'
-  | 'service-fb-instagram'
-  | 'service-light-fb-instagram'
-  | 'service-instagram-fb'
-  | 'service-light-instagram-fb'
-  | 'service-instagram'
-  | 'service-light-instagram'
-  | 'service-twilio'
-  | 'service-light-twilio'
-  | 'service-callback'
-  | 'service-light-callback'
-  | 'service-livechat'
-  | 'service-light-livechat'
-  | 'service-liveid'
-  | 'service-light-liveid'
-  | 'service-ya'
-  | 'service-light-ya'
-  | 'service-ya-dialogs'
-  | 'service-light-ya-dialogs'
-  | 'service-ya-direct'
-  | 'service-light-ya-direct'
-  | 'service-1c'
-  | 'service-light-1c'
-  | 'service-google'
-  | 'service-light-google'
-  | 'service-google-ads'
-  | 'service-light-google-ads'
-  | 'service-twitter'
-  | 'service-light-twitter'
-  | 'service-email'
-  | 'service-light-email'
-  | 'service-messenger'
-  | 'service-light-messenger'
-  | 'service-estore'
-  | 'service-light-estore'
-  | 'service-imessage'
-  | 'service-light-imessage'
-  | 'service-webform'
-  | 'service-light-webform'
-  | 'service-sms'
-  | 'service-light-sms'
-  | 'service-call'
-  | 'service-light-call'
-  | 'service-call-up'
-  | 'service-light-call-up'
-  | 'service-infocall'
-  | 'service-light-infocall'
-  | 'service-site-b24'
-  | 'service-light-site-b24'
-  | 'service-site'
-  | 'service-light-site'
-  | 'service-calltracking'
-  | 'service-light-calltracking'
-  | 'service-telegram'
-  | 'service-light-telegram'
-  | 'service-bitrix24'
-  | 'service-light-bitrix24'
-  | 'service-groupme'
-  | 'service-light-groupme'
-  | 'service-crm'
-  | 'service-light-crm'
-  | 'service-lead'
-  | 'service-light-lead'
-  | 'service-deal'
-  | 'service-light-deal'
-  | 'service-campaign'
-  | 'service-light-campaign'
-  | 'service-chatbot'
-  | 'service-light-chatbot'
-  | 'service-common'
-  | 'service-light-common'
-  | 'service-g-assistant'
-  | 'service-light-g-assistant'
-  | 'service-microsoft'
-  | 'service-light-microsoft'
-  | 'service-office365'
-  | 'service-light-office365'
-  | 'service-skype'
-  | 'service-light-skype'
-  | 'service-webchat'
-  | 'service-light-webchat'
-  | 'service-directline'
-  | 'service-light-directline'
-  | 'service-envelope'
-  | 'service-light-envelope'
-  | 'service-fb-messenger'
-  | 'service-light-fb-messenger'
-  | 'service-fb-adds'
-  | 'service-light-fb-adds'
-  | 'service-outlook'
-  | 'service-light-outlook'
-  | 'service-vk'
-  | 'service-light-vk'
-  | 'service-vk-adds'
-  | 'service-light-vk-adds'
-  | 'service-vk-order'
-  | 'service-light-vk-order'
-  | 'service-fb'
-  | 'service-light-fb'
-  | 'service-fb-comments'
-  | 'service-light-fb-comments'
-  | 'service-slack'
-  | 'service-light-slack'
-  | 'service-viber'
-  | 'service-light-viber'
-  | 'service-kik'
-  | 'service-light-kik'
-  | 'service-apple'
-  | 'service-light-apple'
-  | 'service-universal'
-  | 'service-light-universal'
-  | 'service-organic'
-  | 'service-light-organic'
-  | 'service-telephonybot'
-  | 'service-light-telephonybot'
-  | 'service-whatsapp'
-  | 'service-light-whatsapp'
-  | 'service-wechat'
-  | 'service-light-wechat'
-  | 'service-avito'
-  | 'service-light-avito'
-  | 'service-mailru'
-  | 'service-light-mailru'
-  | 'service-sberbank'
-  | 'service-light-sberbank'
-  | 'service-green-sberbank'
-  | 'service-ok'
-  | 'service-light-ok'
-  | 'service-ya-toloka'
-  | 'service-rest-contact-center'
-  | 'service-light-rest-contact-center'
-  | 'service-audio-infocall'
-  | 'service-light-audio-infocall'
-  | 'service-olx'
-  | 'service-light-olx'
-  | 'service-zoom'
-  | 'service-light-zoom'
-  | 'service-blue-zoom'
-  | 'service-livechat-24'
-  | 'service-widget'
-  | 'service-light-widget'
-  | 'service-wheel'
-  | 'service-light-wheel'
-  | 'service-play'
-  | 'service-light-play'
-  | 'service-other'
-  | 'service-light-other'
-  | 'service-cart'
-  | 'service-light-cart'
-  | 'service-add'
-  | 'service-light-add'
-  | 'service-arrows'
-  | 'service-light-arrows'
-  | 'service-call-in'
-  | 'service-light-call-in'
-  | 'service-call-out'
-  | 'service-light-call-out';
-
-export type TypesProps = {
-  icons: PropIcons[];
-};
-
-export const propsValues: TypesProps = {
-  icons: [
-    'file-empty',
-    'file-txt',
-    'file-doc',
-    'file-xls',
-    'file-php',
-    'file-pdf',
-    'file-ppt',
-    'file-rar',
-    'file-zip',
-    'file-set',
-    'file-img',
-    'file-mov',
-    'file-folder',
-    'file-folder-shared',
-    'file-folder-shared-2',
-    'common-user',
-    'common-user-group',
-    'common-company',
-    'common-phone',
-    'common-notification',
-    'common-bitrix24',
-    'common-cloud',
-    'common-folder',
-    'common-info',
-    'service-alice',
-    'service-light-alice',
-    'service-fb-instagram',
-    'service-light-fb-instagram',
-    'service-instagram-fb',
-    'service-light-instagram-fb',
-    'service-instagram',
-    'service-light-instagram',
-    'service-twilio',
-    'service-light-twilio',
-    'service-callback',
-    'service-light-callback',
-    'service-livechat',
-    'service-light-livechat',
-    'service-liveid',
-    'service-light-liveid',
-    'service-ya',
-    'service-light-ya',
-    'service-ya-dialogs',
-    'service-light-ya-dialogs',
-    'service-ya-direct',
-    'service-light-ya-direct',
-    'service-1c',
-    'service-light-1c',
-    'service-google',
-    'service-light-google',
-    'service-google-ads',
-    'service-light-google-ads',
-    'service-twitter',
-    'service-light-twitter',
-    'service-email',
-    'service-light-email',
-    'service-messenger',
-    'service-light-messenger',
-    'service-estore',
-    'service-light-estore',
-    'service-imessage',
-    'service-light-imessage',
-    'service-webform',
-    'service-light-webform',
-    'service-sms',
-    'service-light-sms',
-    'service-call',
-    'service-light-call',
-    'service-call-up',
-    'service-light-call-up',
-    'service-infocall',
-    'service-light-infocall',
-    'service-site-b24',
-    'service-light-site-b24',
-    'service-site',
-    'service-light-site',
-    'service-calltracking',
-    'service-light-calltracking',
-    'service-telegram',
-    'service-light-telegram',
-    'service-bitrix24',
-    'service-light-bitrix24',
-    'service-groupme',
-    'service-light-groupme',
-    'service-crm',
-    'service-light-crm',
-    'service-lead',
-    'service-light-lead',
-    'service-deal',
-    'service-light-deal',
-    'service-campaign',
-    'service-light-campaign',
-    'service-chatbot',
-    'service-light-chatbot',
-    'service-common',
-    'service-light-common',
-    'service-g-assistant',
-    'service-light-g-assistant',
-    'service-microsoft',
-    'service-light-microsoft',
-    'service-office365',
-    'service-light-office365',
-    'service-skype',
-    'service-light-skype',
-    'service-webchat',
-    'service-light-webchat',
-    'service-directline',
-    'service-light-directline',
-    'service-envelope',
-    'service-light-envelope',
-    'service-fb-messenger',
-    'service-light-fb-messenger',
-    'service-fb-adds',
-    'service-light-fb-adds',
-    'service-outlook',
-    'service-light-outlook',
-    'service-vk',
-    'service-light-vk',
-    'service-vk-adds',
-    'service-light-vk-adds',
-    'service-vk-order',
-    'service-light-vk-order',
-    'service-fb',
-    'service-light-fb',
-    'service-fb-comments',
-    'service-light-fb-comments',
-    'service-slack',
-    'service-light-slack',
-    'service-viber',
-    'service-light-viber',
-    'service-kik',
-    'service-light-kik',
-    'service-apple',
-    'service-light-apple',
-    'service-universal',
-    'service-light-universal',
-    'service-organic',
-    'service-light-organic',
-    'service-telephonybot',
-    'service-light-telephonybot',
-    'service-whatsapp',
-    'service-light-whatsapp',
-    'service-wechat',
-    'service-light-wechat',
-    'service-avito',
-    'service-light-avito',
-    'service-mailru',
-    'service-light-mailru',
-    'service-sberbank',
-    'service-light-sberbank',
-    'service-green-sberbank',
-    'service-ok',
-    'service-light-ok',
-    'service-ya-toloka',
-    'service-rest-contact-center',
-    'service-light-rest-contact-center',
-    'service-audio-infocall',
-    'service-light-audio-infocall',
-    'service-olx',
-    'service-light-olx',
-    'service-zoom',
-    'service-light-zoom',
-    'service-blue-zoom',
-    'service-livechat-24',
-    'service-widget',
-    'service-light-widget',
-    'service-wheel',
-    'service-light-wheel',
-    'service-play',
-    'service-light-play',
-    'service-other',
-    'service-light-other',
-    'service-cart',
-    'service-light-cart',
-    'service-add',
-    'service-light-add',
-    'service-arrows',
-    'service-light-arrows',
-    'service-call-in',
-    'service-light-call-in',
-    'service-call-out',
-    'service-light-call-out',
-  ],
-};
-</script>
-
 <script setup lang="ts">
 import { computed, type PropType } from 'vue';
+import { defaultProps, type PropIcon, type PropSize, propsValues } from './BxIcon.props';
 import { useStyles } from '../composable/useStyles';
+import { getClassName } from '../utils/getClassName';
 
 useStyles();
 
 const props = defineProps({
   icon: {
-    type: String as PropType<PropIcons>,
-    default: 'file-empty',
-    validator(value: PropIcons) {
+    type: String as PropType<PropIcon>,
+    default: defaultProps.icon,
+    validator(value: PropIcon) {
       return propsValues.icons.includes(value);
     },
   },
+  size: {
+    type: String as PropType<PropSize>,
+    default: defaultProps.size,
+    validator(value: PropSize) {
+      return propsValues.sizes.includes(value);
+    },
+  },
+  button: {
+    type: Boolean,
+    default: defaultProps.button,
+  },
+  square: {
+    type: Boolean,
+    default: defaultProps.square,
+  },
 });
 
-const classList = computed(() => ({
-  [`ui-icon-${props.icon}`]: propsValues.icons.includes(props.icon),
-}));
+const classList = computed(() =>
+  getClassName({
+    [`ui-icon-${props.icon}`]: propsValues.icons.includes(props.icon),
+    [`ui-icon-${props.size}`]: propsValues.sizes.includes(props.size),
+    'ui-icon-square': props.square,
+  }),
+);
 </script>
--- src/stories/BxIcon.stories.ts
+++ src/stories/BxIcon.stories.ts
@@ -1,64 +1,38 @@
 import type { Meta, StoryObj } from '@storybook/vue3';
-import BxIcon, { propsValues, type PropIcons } from '../components/BxIcon.vue';
+import { Story } from './Story';
+import BxIcon from '../components/BxIcon.vue';
+import { defaultProps, propsValues, type TypesPropsList } from '../components/BxIcon.props';
 
-type DefaultProps = {
-  icon: PropIcons;
-};
-
-const defaultProps: DefaultProps = {
-  icon: 'file-empty',
-};
-
-const meta = {
+const meta: Meta<typeof BxIcon> = {
   title: 'bx-icon',
   component: BxIcon,
   args: {
     icon: defaultProps.icon,
+    size: defaultProps.size,
+    button: defaultProps.button,
+    square: defaultProps.square,
   },
   argTypes: {
     icon: {
       options: propsValues.icons,
-      defaultValue: defaultProps.icon,
       control: { type: 'select' },
     },
+    size: {
+      options: propsValues.sizes,
+      control: { type: 'select' },
+    },
+    button: {
+      control: { type: 'boolean' },
+    },
+    square: {
+      control: { type: 'boolean' },
+    },
   },
-} satisfies Meta<typeof BxIcon>;
-
-export default meta;
+};
 
 type StoryType = StoryObj<typeof meta>;
 
-export const Default: StoryType = {
-  render: (args) => ({
-    template: '<bx-icon v-bind="args" v-on="args">{{ args.default }}</bx-icon>',
-    data: () => ({ args }),
-    provide: {
-      $BX24: null,
-    },
-    components: { BxIcon },
-  }),
-};
-
-const Story = (propName: string, propList: any[]) => ({
-  render: (args: any) => ({
-    template: `
-      <div v-for="item in propList" :key="item" class="component">
-        <bx-icon v-bind="args" v-on="args" :[propName]="item">{{ args.default }}</bx-icon>
-      </div>
-    `,
-    data: () => ({ args, propName, propList }),
-    provide: {
-      $BX24: null,
-    },
-    components: { BxIcon },
-  }),
-  argTypes: {
-    [propName]: {
-      table: {
-        disable: true,
-      },
-    },
-  },
-});
-
-export const Icons: StoryType = Story('icon', propsValues.icons);
+export default meta;
+export const Default: StoryType = Story(BxIcon);
+export const Icons: StoryType = Story<TypesPropsList>(BxIcon, 'icon', propsValues.icons);
+export const Sizes: StoryType = Story<TypesPropsList>(BxIcon, 'size', propsValues.sizes);
