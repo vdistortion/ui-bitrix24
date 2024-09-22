c7695b6497b9811c9ae20cb0dc3f15b117f763dd
Валентин <valentin.distortion@gmail.com>
2024-09-22 16:19:51 +0500

    BxTagSelector

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,9 +1,28 @@
 <template>
-  <div></div>
+  <div style="display: flex; flex-direction: column; gap: 20px">
+    <bx-dialog></bx-dialog>
+    <bx-tag-selector :tags="users" placeholder="placeholder"></bx-tag-selector>
+    <bx-tag-selector :tags="users" hidden-text-box></bx-tag-selector>
+    <bx-entity-selector></bx-entity-selector>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { inject } from 'vue';
+import BxDialog from './components/BxDialog.vue';
+import BxTagSelector from './components/BxTagSelector.vue';
+import BxEntitySelector from './components/BxEntitySelector.vue';
+
+const users = [
+  {
+    link: '/company/personal/user/150/',
+    name: 'Администратор Админ',
+  },
+  {
+    link: '/company/personal/user/150/',
+    name: 'Администратор Админ',
+  },
+];
 
 const $BX24 = inject('$BX24');
 console.log({ $BX24 });
--- vue/src/components/BxTagSelector.vue
+++ vue/src/components/BxTagSelector.vue
@@ -1,35 +1,43 @@
 <template>
-  <!--    https://dev.1c-bitrix.ru/api_d7/bitrix/ui/entity_selector/tag_selector/index.php-->
   <div class="ui-tag-selector-outer-container">
     <div class="ui-tag-selector-container">
       <div class="ui-tag-selector-items">
         <div
+          v-for="tag in props.tags"
           class="ui-tag-selector-item ui-tag-selector-tag ui-tag-selector-tag--has-avatar"
-          style="background-color: rgb(188, 237, 252)"
         >
-          <a class="ui-tag-selector-tag-content" href="/company/personal/user/150/" target="_blank">
+          <a class="ui-tag-selector-tag-content" :href="tag.link" target="_blank">
             <div
               class="ui-tag-selector-tag-avatar"
               :style="`background-image: url(${getImageUrl('default-tag-user')});`"
             ></div>
             <div class="ui-tag-selector-tag-title" style="color: rgb(16, 102, 187)">
-              Создаватель Админ
+              {{ tag.name }}
             </div>
           </a>
           <div class="ui-tag-selector-tag-remove"></div>
         </div>
         <input
           type="text"
-          class="ui-tag-selector-item ui-tag-selector-text-box ui-tag-selector-item-hidden"
+          class="ui-tag-selector-item ui-tag-selector-text-box"
+          :class="{
+            'ui-tag-selector-item-hidden': props.hiddenTextBox,
+          }"
           autocomplete="off"
-          placeholder=""
+          :placeholder="props.placeholder"
           value=""
-        /><span class="ui-tag-selector-item ui-tag-selector-add-button"
-          ><span class="ui-tag-selector-add-button-caption">Добавить</span></span
-        >
+        />
+        <span class="ui-tag-selector-item ui-tag-selector-add-button">
+          <span class="ui-tag-selector-add-button-caption">{{ props.textAddButton }}</span>
+        </span>
       </div>
-      <div class="ui-tag-selector-create-button ui-tag-selector-item-hidden">
-        <span class="ui-tag-selector-create-button-caption">Создать</span>
+      <div
+        class="ui-tag-selector-create-button"
+        :class="{
+          'ui-tag-selector-item-hidden': props.hiddenCreateButton,
+        }"
+      >
+        <span class="ui-tag-selector-create-button-caption">{{ props.textCreateButton }}</span>
       </div>
     </div>
   </div>
@@ -39,6 +47,33 @@
 import { loadStyles } from '../utils/loadStyles';
 
 loadStyles();
+
+const props = defineProps({
+  tags: {
+    type: Array,
+    default: () => [],
+  },
+  placeholder: {
+    type: String,
+    default: '',
+  },
+  textAddButton: {
+    type: String,
+    default: 'Добавить',
+  },
+  textCreateButton: {
+    type: String,
+    default: 'Создать',
+  },
+  hiddenCreateButton: {
+    type: Boolean,
+    default: false,
+  },
+  hiddenTextBox: {
+    type: Boolean,
+    default: false,
+  },
+});
 
 function getImageUrl(name: string) {
   return new URL(`../assets/${name}.svg`, import.meta.url).href;
