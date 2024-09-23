160be39497a2c1b5daf1737843471d5818317efd
Валентин <valentin.distortion@gmail.com>
2024-09-24 00:13:15 +0500

    BxDialog refactor

--- common/utils/loadStyles.ts
+++ common/utils/loadStyles.ts
@@ -3,21 +3,22 @@ import './font.open-sans.css';
 
 export const loadStyles = (BX24?: IBitrix24Library) => {
   const domain = BX24 ? BX24.getDomain() : 'www.bitrix24.ru';
-  const root = `https://${domain}/bitrix/js/ui`;
+  const root = `https://${domain}/bitrix/js`;
   const files = [
-    'design-tokens/dist/ui.design-tokens.min.css',
-    'alerts/ui.alerts.min.css',
-    'buttons/ui.buttons.min.css',
-    'buttons/icons/ui.buttons.icons.min.css',
-    'entity-selector/dist/entity-selector.bundle.min.css',
-    'forms/ui.forms.min.css',
-    'hint/ui.hint.min.css',
-    'icons/b24/ui.icons.b24.min.css',
-    'icons/base/ui.icons.base.min.css',
-    'icons/disk/ui.icons.disk.min.css',
-    'icons/service/ui.icons.service.min.css',
-    'layout-form/dist/layout-form.bundle.min.css',
-    'progressbar/ui.progressbar.min.css',
+    'ui/design-tokens/dist/ui.design-tokens.min.css',
+    'ui/alerts/ui.alerts.min.css',
+    'ui/buttons/ui.buttons.min.css',
+    'ui/buttons/icons/ui.buttons.icons.min.css',
+    'ui/entity-selector/dist/entity-selector.bundle.min.css',
+    'ui/forms/ui.forms.min.css',
+    'ui/hint/ui.hint.min.css',
+    'ui/icons/b24/ui.icons.b24.min.css',
+    'ui/icons/base/ui.icons.base.min.css',
+    'ui/icons/disk/ui.icons.disk.min.css',
+    'ui/icons/service/ui.icons.service.min.css',
+    'ui/layout-form/dist/layout-form.bundle.min.css',
+    'ui/progressbar/ui.progressbar.min.css',
+    'main/popup/dist/main.popup.bundle.min.css',
   ];
   const styles = files.reduce((Fragment, file) => {
     const href = [root, file].join('/');
--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,34 +1,30 @@
 <template>
-  <div style="display: flex; flex-direction: column; gap: 20px">
-    <bx-dialog></bx-dialog>
-    <bx-tag-selector :list="users" display-field-name="name">
-      <BxIcon icon="common-user" size="xs"></BxIcon>
-    </bx-tag-selector>
-    <bx-tag-selector
-      :list="users"
-      display-field-name="name"
-      icon
-      default-icon="default-user"
-    ></bx-tag-selector>
+  <div style="display: flex; flex-direction: column; gap: 20px; margin-left: 200px">
+    <bx-dialog visible-footer>
+      <bx-tag-selector
+        :list="users"
+        display-field-name="name"
+        display-field-icon="icon"
+        icon
+        placeholder="поиск"
+        show-input-text
+        :show-add-button="false"
+      ></bx-tag-selector>
+    </bx-dialog>
   </div>
 </template>
 
 <script setup lang="ts">
 import { inject } from 'vue';
-import BxIcon from './components/BxIcon.vue';
 import BxDialog from './components/BxDialog.vue';
 import BxTagSelector from './components/BxTagSelector.vue';
 import type { ItemType } from './components/BxTagSelector.props';
 
 const users: ItemType[] = [
   {
-    link: '/company/personal/user/150/',
-    name: 'Администратор',
-  },
-  {
-    link: '/company/personal/user/151/',
-    name: 'Админ',
-    icon: 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_light_color_272x92dp.png',
+    link: '/company/personal/user/648/',
+    name: 'Валентин Золотов',
+    icon: 'https://cdn-ru.bitrix24.ru/teamr/resize_cache/406111/7acf4cadf975128573a8b1c2766af5d8/main/083/083bb1c93b3f0ea32baf3942a5ac6831/T02A7V6EGBA-U02AWPPB8KW-8358c0c61795-512.png',
   },
 ];
 
